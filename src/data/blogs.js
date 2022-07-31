3ba00c51a697470b5180b283bf611fff8dd9ef4d
Rishabh Kunwar <74833182+kunwar-11@users.noreply.github.com>
2022-07-31 09:55:17 +0530

    Update blogs.js

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -15,9 +15,7 @@ export const blogs = [
 
     bId: ++id,
 
-    coverPage:
-
-      "https://images.app.goo.gl/e467TS4TdFhr9c9q8",
+    coverPage:"https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1647407453587%2FvjBg9uVKz.jpg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
 
     topicName: "The Toolkit Saga",
 
